93d23d025aa98a7179c447d3ee82d1836ee937d8
Greg <gboruszewski@gmail.com>
2020-07-21 16:16:47 +0200

    allow searching after search button click - depending on configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import SearchBar from "./components/SearchBar/SearchBar";
 import SearchResults from "./components/SearchResults/SearchResults";
+import { Button } from "react-bootstrap";
 import './App.scss';
 
 const REPO_URL = "https://api.github.com/search/users";
 const TIMEOUT_IN_MILISEC = 1000;
+const SHOW_SEARCH_BUTTON = true;
 
 export default class App extends Component {
 
@@ -12,19 +14,52 @@ export default class App extends Component {
     super(props);
     this.state = {
       search: "",
+      searchSaved: "",
       results: [],
+      isSearching: false,
+      searchFinished: false,
     }
     this.updateSearch = this.updateSearch.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.startSearch = this.startSearch.bind(this);
+    this.finishSerch = this.finishSearch.bind(this);
     this.setResults = this.setResults.bind(this);
+    this.renderSearchButton = this.renderSearchButton.bind(this);
+    this.onSearchButtonClick = this.onSearchButtonClick.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
   timeoutId = "";
 
+  startSearch() {
+    // everything that should happen immediately after changing the search word
+    this.setState({
+      isSearching: true,
+    });
+  }
+
+  finishSearch() {
+    this.setState({
+      isSearching: false,
+      searchFinished: true,
+    });
+  }
+
   handleSearch() {
-    const url = `${REPO_URL}?q=${this.state.search}`
-    fetch(url)
-    .then(data => data.json())
-    .then(data => this.setResults(data.items));
+    const { search, searchSaved } = this.state;
+    const searchword = SHOW_SEARCH_BUTTON ? searchSaved : search;
+    if(searchword) {
+      const url = `${REPO_URL}?q=${searchword}`;
+      fetch(url)
+      .then(data => data.json())
+      .then(data => this.setResults(data.items))
+      .catch( error => console.warn(error))
+      .finally( () => this.finishSearch());
+    } else {
+      this.setState({
+        results:[],
+      });
+      this.finishSearch();
+    }
   }
 
   setResults(data) {
@@ -35,10 +70,10 @@ export default class App extends Component {
   }
 
   parseResults(data) {
-    return data.map( item => ({
+    return data ? data.map( item => ({
       name: item.login,
       url: item.repos_url,
-    }))
+    })) : [];
   }
 
   manageSearchTimeout() {
@@ -50,16 +85,37 @@ export default class App extends Component {
 
   updateSearch(search) {
     this.setState({search: search}, () => {
-      this.manageSearchTimeout();
+      !SHOW_SEARCH_BUTTON && this.manageSearchTimeout();
     });
   }
 
+  onSearchChange(search) {
+    !SHOW_SEARCH_BUTTON && this.startSearch();
+    this.updateSearch(search);
+  }
+
+  onSearchButtonClick() {
+    this.setState(state => ({
+      searchSaved: state.search,
+    }), () => {
+      this.startSearch();
+      this.handleSearch();
+    })
+  }
+
+  renderSearchButton() {
+    return <Button onClick={this.onSearchButtonClick}>Search</Button>
+  }
+
   render() {
-    const { state: { search, results }, updateSearch } = this;
+    const { state: { search, searchSaved, results, isSearching, searchFinished }, onSearchChange, renderSearchButton } = this;
+    const shouldShowLabel = !SHOW_SEARCH_BUTTON || searchFinished;
+    const searchWord = SHOW_SEARCH_BUTTON ? searchSaved : search;
     return (
       <>
-        <SearchBar value={search} onChange={updateSearch} />
-        <SearchResults results={results} />
+        <SearchBar value={search} onChange={onSearchChange} />
+        {SHOW_SEARCH_BUTTON && renderSearchButton()}
+        <SearchResults searchKey={searchWord} results={results} isSearching={isSearching} showLabel={shouldShowLabel} />
       </>
     )
   }
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-export default function SearchResults({results}) {
-
+export default function SearchResults({results,searchKey,isSearching,showLabel}) {
   return (
     <div>
-      {results.map( (result,index) => <li key={index}>{result.name}</li>)}
+      {
+        isSearching ? "Searching..."
+        :
+        <>
+          {showLabel && <h4>
+            {searchKey && results.length ? `Showing users for ${searchKey}` : ( searchKey ? 'No results found' : 'No search key provided' )}
+          </h4>}
+          {results.map( (result,index) => <li key={index}>{result.name}</li>)}
+        </>
+    }
     </div>
   )
 }
